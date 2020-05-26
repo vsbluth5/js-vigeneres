1ab069e088cf84ac0f89ddc4f1f285e097636fe8
Glitch (hello-webpage) <none>
2020-05-26 16:57:53 +0000

    🛩😂 Checkpoint ./script.js:3856783/619

--- script.js
+++ script.js
@@ -4,9 +4,25 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-const message = document.querySelector("text-area")
-console.log(message)
-const key = document.querySelector("text-a")
-console.log(message)
-const key = document.querySelector("input")
-console.log(key)
+const messageArea = document.querySelector("textarea")
+// console.log(messageArea)
+const encryptBtn= document.querySelector("button")
+// console.log(encryptBtn)
+const keyInput = document.querySelector("input")
+// console.log(keyInput)
+const encryption = document.querySelector("#encryption")
+// console.log(encryption)
+
+encryptBtn.addEventListener("click", (e)=>{
+  // console.log("Encode blicked")
+  encryption.innerHTML = `Encode has been clicked`
+  // Get the message from the messageArea
+  encryption.innerHTML += `${messageArea.}`
+  // Get the key from keyInput
+  encryption.innerHTML += `Encode has been clicked`
+  
+  
+  
+  encryption.innerHTML = `Encode has been clicked`
+  
+})
